ca12af21c8b4dce14f8558d846ec181fa4de4ee1
Joshua Murphy <hello@joshmurphy.ca>
2023-08-24 14:20:01 -0230

    Add Meson and Nix shell

---
+++ shell.nix
@@ -0,0 +1,20 @@
+{ pkgs ? import <nixpkgs> { } }:
+pkgs.mkShell {
+  nativeBuildInputs = with pkgs.buildPackages; [
+    cmake
+    jack2
+    meson
+    ninja
+    fluidsynth
+    pulseaudio
+    alsa-lib
+    libsndfile
+    pcre2
+    glib
+    pkg-config
+    clang
+    SDL2
+    SDL2_mixer
+    SDL2_net
+  ];
+}
