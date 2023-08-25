85f4beecc299792c9afc3ca62d92d800bf503f88
Joshua Murphy <hello@joshmurphy.ca>
2023-08-25 12:37:47 -0230

    Remove Win32 opendir implementation

---
+++ config.h
@@ -0,0 +1,12 @@
+#define PACKAGE_NAME "Mindoom"
+#define PACKAGE_TARNAME "mindoom"
+#define PACKAGE_VERSION "0.0.3"
+#define PACKAGE_STRING "Mindoom 0.0.3"
+
+#define PROGRAM_PREFIX "min"
+
+/* #undef HAVE_LIBSAMPLERATE */
+/* #undef HAVE_LIBPNG */
+#define HAVE_DIRENT_H
+#define HAVE_DECL_STRCASECMP 1
+#define HAVE_DECL_STRNCASECMP 1
